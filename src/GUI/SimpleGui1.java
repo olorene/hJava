02796a8aaeace6cddfb8aa0dfc02f4abf0e79ce2
olorene <ggarik83@gmail.com>
2019-01-02 17:20:22 +0200

    add midi code

---
+++ src/GUI/SimpleGui1.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import oracle.jrockit.jfr.JFR;
+
+import javax.swing.*;
+
+public class SimpleGui1 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Cleck me");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(button);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+
+    }
+}
