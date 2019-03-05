25cc6b62bc77f2dcf71a4ecc3cec76cca33a3227
olorene <ggarik83@gmail.com>
2019-03-05 22:25:14 +0200

    Add some method Go

--- src/Swing/Widgets_My.java
+++ src/Swing/Widgets_My.java
@@ -10,13 +10,17 @@ public class Widgets_My implements ActionListener {
     JButton button = new JButton("Click my");
     JCheckBox checkBox = new JCheckBox("Choose me");
     JTextField textField = new JTextField("This is a text field");
+    JTextArea textArea;
+    JLabel label = new JLabel("My names");
     JPanel panelLeft = new JPanel();
     JPanel panelRight = new JPanel();
     JPanel panelSouth = new JPanel();
+    JPanel panel = new JPanel();
+    JButton buttonGo4;
 
     public static void main(String[] args) {
         Widgets_My widgets_my = new Widgets_My();
-        widgets_my.go4();
+        widgets_my.go7();
     }
 
     public void go() {
@@ -59,19 +63,54 @@ public class Widgets_My implements ActionListener {
     public void go4() {
         JFrame frameGo4 = new JFrame();
         JPanel panelGo4 = new JPanel();
-        panelGo4.setBackground(Color.DARK_GRAY);
+        buttonGo4 = new JButton("It's my button");
 
-        JButton buttonGo4 = new JButton("This is RED");
+        frameGo4.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        buttonGo4.addActionListener(this);
+
+        panelGo4.setBackground(Color.RED);
         panelGo4.add(buttonGo4);
-
-        goStart();
         frameGo4.getContentPane().add(BorderLayout.EAST, panelGo4);
+
+        frameGo4.setSize(250, 200);
+        frameGo4.setVisible(true);
+    }
+
+    public void go5() {
+        goStart();
+        panel.setBackground(Color.PINK);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        buttonGo4 = new JButton("Button 4");
+        panel.add(button);
+        panel.add(buttonGo4);
+        frame.getContentPane().add(BorderLayout.EAST, panel);
         goEnd();
     }
 
-    private void goEnd() {
-        frame.setSize(300, 300);
-        frame.setVisible(true);
+    public void go6() {
+        goStart();
+        panel.add(label);
+        panel.add(textField);
+        panel.add(button);
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        goEnd();
+    }
+
+    public void go7() {
+        goStart();
+
+        textArea = new JTextArea(10, 20);
+        textArea.setLineWrap(true);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        panel.add(scrollPane);
+
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+
+        goEnd();
     }
 
     private void goStart() {
@@ -79,8 +118,16 @@ public class Widgets_My implements ActionListener {
         button.addActionListener(this);
     }
 
+    private void goEnd() {
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        button.setText("I've been clicked");
+//        button.setText("I've been clicked");
+//        textField.setText("  Aloha!!!  ");
+        textArea.append("button clicked \n");
+
     }
 }
