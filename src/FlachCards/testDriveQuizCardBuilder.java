36bb5559ea0540fb9a3e9b4b3478f9b14e467ea2
olorene <ggarik83@gmail.com>
2019-03-14 03:30:53 +0200

    Commit testDriveQuizCardBuilder

---
+++ src/FlachCards/testDriveQuizCardBuilder.java
@@ -0,0 +1,8 @@
+package FlachCards;
+
+public class testDriveQuizCardBuilder {
+    public static void main(String[] args) {
+        QuizCardBuilder quizCardBuilder = new QuizCardBuilder();
+        quizCardBuilder.go();
+    }
+}
