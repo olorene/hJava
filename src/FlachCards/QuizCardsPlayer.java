9d6c70c5a585b04524123883b2964f57cd8597cc
olorene <ggarik83@gmail.com>
2019-03-14 11:49:55 +0200

    Start QuizCardsPlayer

--- src/FlachCards/QuizCardsPlayer.java
+++ src/FlachCards/QuizCardsPlayer.java
@@ -1,4 +1,42 @@
 package FlachCards;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 public class QuizCardsPlayer {
+
+    private JTextArea display;
+    private JTextArea answer;
+    private ArrayList<QuizCard> cardList;
+    private QuizCard currentCard;
+    private JFrame frame;
+    private JButton nextButton;
+    private boolean isShowAnswer;
+
+    public static void main(String[] args) {
+        QuizCardsPlayer reader = new QuizCardsPlayer();
+        reader.go();
+    }
+
+    public void go(){
+
+    }
+
+    public class NextCardListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    public class OpenMenuList implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+
 }
