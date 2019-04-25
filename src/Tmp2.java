1b7feb2d55a7aef37d394879581e7dfc8fc804a0
olorene <ggarik83@gmail.com>
2019-04-25 14:44:13 +0300

    A class Tmp2 for traning

---
+++ src/Tmp2.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Tmp2 {
+    public void first(String str) {
+        System.out.println(str);
+    }
+    public void data(){
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
