d8c4bfa0ed88b5966956214299d3aa02a1a5b48d
olorene <ggarik83@gmail.com>
2019-04-26 11:42:42 +0300

    Working version connection Jsoup tortuga

--- src/Jsoap/LoginToTortuga.java
+++ src/Jsoap/LoginToTortuga.java
@@ -1,5 +1,6 @@
 package Jsoap;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,32 +13,57 @@ import java.util.Map;
 
 public class LoginToTortuga {
     public static void main(String[] args) {
-        String userAgent = "Mozilla/5.0";
+        Map<String, String> cookies;
+        Connection.Response response;
+        String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36";
         try {
             String token = "";
             //grab login form page first
-            Connection.Response response = Jsoup
+            response = Jsoup
                     .connect("http://tortuga-gamestable.top/admin/index.php?route=common/login")
                     .referrer("http://www.google.com.ua/")
                     .userAgent(userAgent)
                     .timeout(10 * 1000)
+                    .execute();
+
+
+
+            cookies = response.cookies();
+            response = Jsoup
+                    .connect("http://tortuga-gamestable.top/admin/index.php?route=common/login")
+                    .referrer("http://www.google.com.ua/")
+                    .data("username", "!!!!!")
+                    .data("password", "!!!!!")
+                    .userAgent(userAgent)
+                    .timeout(10 * 1000)
+                    .cookies(cookies)
+                    .method(Connection.Method.POST)
 //                    .followRedirects(true)
                     .execute();
 
-                Document doc = response.parse();
-            for (Element meta : doc.select("meta")) {
-                if (meta.attr("name").equals("csrf-token")) {
-                    token = meta.attr("content");
-                }
-            }
-            System.out.println(token);
+            Document doc = response.parse();
+            System.out.println(doc.title());
+//            cookies.putAll(response.cookies());
+            System.out.println(cookies);
+
+
+
+
+
+
+//            for (Element meta : doc.select("meta")) {
+//                if (meta.attr("name").equals("csrf-token")) {
+//                    token = meta.attr("content");
+//                }
+//            }
+//            System.out.println(token);
 //            //get the cookies from the response, which we will post to the action URL
 //            Map<String, String> mapLoginPageCookies = response.cookies();
 //
 //            //lets make data map containing all the parameters and its values found in the form
 //            Map<String, String> mapParams = new HashMap<String, String>();
-//            mapParams.put("username", "Lordus");
-//            mapParams.put("password", "vorlon2258");
+//            mapParams.put("username", "!!!!!");
+//            mapParams.put("password", "!!!!!!");
 //            mapParams.put("redirect", "http://tortuga-gamestable.top/admin/index.php?route=common/login");
 //           //URL found in form's action attribute
 //            String strActionURL = "http://tortuga-gamestable.top/admin/index.php?route=common/login";
@@ -73,5 +99,7 @@ public class LoginToTortuga {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+
     }
 }
