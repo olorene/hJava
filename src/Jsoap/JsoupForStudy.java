a5c4be9a0da103c357be5143dc8133b571d46a0c
olorene <ggarik83@gmail.com>
2019-05-02 09:03:53 +0300

    Jsoup For Study2

--- src/Jsoap/JsoupForStudy.java
+++ src/Jsoap/JsoupForStudy.java
@@ -3,12 +3,51 @@ package Jsoap;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class JsoupForStudy {
     public static void main(String[] args) {
-        String html = "<html><head><title>First parse</title></head>"
-                + "<body><p>Parsed HTML into a doc.</p></body></html>";
+        String html =
+                "<html>" +
+                    "<head>" +
+                        "<title>First parse</title>" +
+                    "</head>" +
+                    "<body>" +
+                        "<p>Parsed HTML into a doc.</p>" +
+                        "<table>" +
+                            "<tr>" +
+                                "<td> " +
+                                    "<input type=\"checkbox\"> " +
+                                "</td>" +
+                                "<td> Dark Angel </td>" +
+                                "<td> Blood Angel </td>" +
+                            "</tr>" +
+                                "<tr>" +
+                                "<td> " +
+                                    "<input type=\"checkbox\"> " +
+                                "</td>" +
+                                "<td> Dark Angel2 </td>" +
+                                "<td> Blood Angel2 </td>" +
+                            "</tr>" +
+                        "</table>" +
+                    "</body>" +
+                "</html>";
         Document doc = Jsoup.parse(html);
-        System.out.println(doc);
+        Elements table = doc.select("table");
+        Elements td = table.select("td");
+//        System.out.println(td);
+        for (int i = 0; i < td.size(); i++) {
+            Element tdElement = td.get(i);
+            System.out.print(tdElement.text() + "= ");
+        }
+//        Elements rows = table.select("tr");
+//
+//        for(int i = 0; i < rows.size(); i++) {
+//            String forPrint = rows.outerHtml() + " = ";
+//            System.out.println(forPrint);
+//        }
+
+//        System.out.println(doc);
     }
 }
